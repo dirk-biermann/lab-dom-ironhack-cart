ef0c78a9010805e298acb91489fd4ea2b42a0617
Biermann, Dirk <biermannd@mediamarktsaturn.com>
2019-11-27 13:54:59 +0100

    first iteration

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,36 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
+var $inp = document.querySelector('#cart tbody .qty input');
+$inp.onchange = updateSubtot;
+
+// ----------------------------------------------------
 
 function updateSubtot($product) {
   // Iteration 1.1
+  let quantity = parseFloat( document.querySelector('.qty input').value );
+  let priceUnit = parseFloat( document.querySelector('.pu span').innerText );
+
+  let subTotal = ( quantity * priceUnit );
+  document.querySelector('.subtot span').innerText = subTotal.toFixed(2);
+  
+  return subTotal;
+}
+
+function getSubtotals() {
+  // Iteration 1.1
+  let totalPrice = 0.00;
+
+  let carts = document.querySelectorAll('tbody tr' );
+  carts.forEach(element => {
+    totalPrice += updateSubtot( element );
+  });
+  console.log( "Total:", totalPrice);
 }
 
 function calcAll() {
   // Iteration 1.2
+  getSubtotals();
 }
+
+
 $calc.onclick = calcAll;
